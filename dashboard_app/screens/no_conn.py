8214ba999ebf6ded5211bf29cd0616a4aafaa189
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2020-07-10 16:50:08 +0200

    fixed a back btn system bug and added unschduling to always check for internet event

--- dashboard_app/screens/no_conn.py
+++ dashboard_app/screens/no_conn.py
@@ -2,11 +2,11 @@ from kivy.clock import Clock
 from screens.template_screen import template_screen
 
 class no_conn(template_screen):
-    def on_enter(self,*args):
-        self.utils.current_screen(self.name)
-        Clock.schedule_interval(self.check_for_conn,.5)
+    def on_pre_enter(self,*args):
+        self.check_event=Clock.schedule_interval(self.check_for_conn,.5)
         
     def check_for_conn(self,dt):
         if self.api.check_for_internet():
             self.manager.current = self.utils.prev_screen()
+            Clock.unschedule(self.check_event)
         return
--- dashboard_app/utils.py
+++ dashboard_app/utils.py
@@ -4,6 +4,8 @@ class utils:
     def __init__(self):
         self.email_pat=re.compile('^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$')
         self.screen_stack=[]
+        # Screenmanager loads the first screen registered to it so we are just going to go ahead and skip that tehe (since it fucks up the Back btn logic)
+        self.first_call=True
         return
 
     def validate_email(email):
@@ -11,9 +13,17 @@ class utils:
             return True
         else:
             return False
+
     def current_screen(self,sname):
+        if self.first_call:
+            self.first_call=False
+            return
         self.screen_stack.append(sname)
 
     def prev_screen(self):
-        print(self.screen_stack)
-        return self.screen_stack[len(self.screen_stack)-2]
+        r_idx=len(self.screen_stack)-2
+        if r_idx<0:
+            return "map_screen"
+        prev_name=self.screen_stack[r_idx]
+        del self.screen_stack[r_idx]
+        return prev_name
