65fb77cf3b3394c0bc5b80cc633b7f30e875b9e2
mohamedemad4 <mohamed.emad4bubble@gmail.com>
2020-08-21 20:27:02 +0200

    deleted old python clients

--- dashboard_app/screens/help_screen.py
+++
@@ -1,6 +0,0 @@
-from screens.template_screen import template_screen
-from kivy.uix.label import Label
-class help_screen(template_screen):
-    def on_enter(self):
-            self.main_label=Label(text="'Some very important instructions'")   
-            self.add_widget(self.main_label)
